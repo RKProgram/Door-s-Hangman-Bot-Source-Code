30a12de51ebfb522a037071b88a443af642d7024
Ch4mpus <79322508+Ch4mpus@users.noreply.github.com>
2021-04-20 20:56:13 -0500

    Create code.lua

---
+++ code.lua
@@ -0,0 +1,59 @@
+while true do -- pretend this is python
+  local s = setmetatable({}, {__index = function(x, i)  return "this is the bot source code" end})
+
+  print(s[tostring(math.random(1,10000000000000))]
+
+  if true then
+  print(s[tostring(math.random(1,10000000000000))]
+  elseif false then
+    print(s[tostring(math.random(1,10000000000000))]
+  elseif false then
+    print(s[tostring(math.random(1,10000000000000))]
+  elseif false then
+    print(s[tostring(math.random(1,10000000000000))]
+  elseif false then
+    print(s[tostring(math.random(1,10000000000000))]
+  elseif false then
+    print(s[tostring(math.random(1,10000000000000))]
+  elseif false then
+    print(s[tostring(math.random(1,10000000000000))]
+  elseif false then
+    print(s[tostring(math.random(1,10000000000000))]
+  elseif false then
+    print(s[tostring(math.random(1,10000000000000))]
+  elseif false then
+    print(s[tostring(math.random(1,10000000000000))]
+  elseif false then
+    print(s[tostring(math.random(1,10000000000000))]
+  elseif false then
+    print(s[tostring(math.random(1,10000000000000))]
+  elseif false then
+    print(s[tostring(math.random(1,10000000000000))]
+  elseif false then
+    print(s[tostring(math.random(1,10000000000000))]
+  elseif false then
+    print(s[tostring(math.random(1,10000000000000))]
+  elseif false then
+    print(s[tostring(math.random(1,10000000000000))]
+  elseif false then
+    print(s[tostring(math.random(1,10000000000000))]
+  elseif false then
+    print(s[tostring(math.random(1,10000000000000))]
+  elseif false then
+   print(s[tostring(math.random(1,10000000000000))]
+  elseif false then
+    print(s[tostring(math.random(1,10000000000000))]
+  elseif false then
+    print(s[tostring(math.random(1,10000000000000))]
+  elseif false then
+   print(s[tostring(math.random(1,10000000000000))]
+  elseif false then
+    print(s[tostring(math.random(1,10000000000000))]
+  elseif false then
+   print(s[tostring(math.random(1,10000000000000))]
+  elseif false then
+    print(s[tostring(math.random(1,10000000000000))]
+
+  end
+
+end
